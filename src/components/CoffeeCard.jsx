fe72f6d089f6dfc537983039cacfef1cacd3f18d
Arman <mdarman365506@gmail.com>
2024-11-26 17:06:18 +0600

    added edit option

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,40 +1,76 @@
-
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-    const {
-        name,
-        quantity,
-        suplier,
-        taste,
-        category,
-        details,
-        photo,
-    } = coffee;
-    // console.log(coffee);
-    return (
-      <div>
-        <div className="card card-side bg-base-100 shadow-xl">
-          <figure>
-            <img src={photo} alt="Movie" />
-          </figure>
-          <div className="flex justify-between border-2 w-full pr-4">
-            <div>
-              <h2 className="card-title">Name: {name}</h2>
-              <p>{quantity}</p>
-              <p>{suplier}</p>
-              <p>{taste}</p>
-            </div>
-            <div className="card-actions justify-end">
-              <div className="join join-vertical space-y-4">
-                <button className="btn btn-primary">View</button>
+  const { _id, name, quantity, suplier, taste, category, details, photo } =
+    coffee;
+//   console.log(coffee);
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log("delete confirmed");
+
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img src={photo} alt="Movie" />
+        </figure>
+        <div className="flex justify-between border-2 w-full pr-4">
+          <div>
+            <h2 className="card-title">Name: {name}</h2>
+            <p>{quantity}</p>
+            <p>{suplier}</p>
+            <p>{taste}</p>
+          </div>
+          <div className="card-actions justify-end">
+            <div className="join join-vertical space-y-4">
+              <button className="btn btn-primary">View</button>
+              <Link to={`/updateCoffee/${_id}`}>
                 <button className="btn btn-primary">Edit</button>
-                <button className="btn btn-primary">X</button>
-              </div>
+              </Link>
+              <button
+                onClick={() => {
+                  handleDelete(_id);
+                }}
+                className="btn btn-error text-white"
+              >
+                X
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default CoffeeCard;
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,9 +1,162 @@
+import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateCoffee = () => {
+    const coffee = useLoaderData();
+    console.log(coffee);
+    const { _id, name, quantity, suplier, taste, category, details, photo } =
+      coffee;
+
+    
+ const handleUpdateCoffee = (event) => {
+   event.preventDefault();
+   const form = event.target;
+
+   const name = form.name.value;
+   const quantity = form.quantity.value;
+   const suplier = form.suplier.value;
+   const taste = form.taste.value;
+   const category = form.category.value;
+   const details = form.details.value;
+   const photo = form.photo.value;
+
+   const updateCoffee = {
+     name,
+     quantity,
+     suplier,
+     taste,
+     category,
+     details,
+     photo,
+   };
+   console.log(updateCoffee);
+
+   fetch(`http://localhost:5000/coffee/${_id}`, {
+     method: "PUT",
+     headers: {
+       "content-type": "application/json",
+     },
+     body: JSON.stringify(updateCoffee),
+   })
+     .then((res) => res.json())
+     .then((data) => {
+       console.log(data);
+       Swal.fire({
+         title: "Success!",
+         text: "Coffee added successfully",
+         icon: "success",
+         confirmButtonText: "Cool",
+       });
+     });
+ };
+
     return (
-        <div>
-            Update Coffee
-        </div>
+      <div className="container mx-auto bg-[#F4F3F0] p-24">
+        <h2 className="text-2xl font-bold">Update Coffee: {name}</h2>
+        <form onSubmit={handleUpdateCoffee} action="#" className="space-y-4">
+          {/* form row */}
+          <div className="md:flex">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Coffee Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Coffee Name"
+                defaultValue={name}
+                className="input input-bordered w-full "
+              />
+            </div>
+            <div className="form-control md:w-1/2 ml-4">
+              <label className="label">
+                <span className="label-text">Available Quantity</span>
+              </label>
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                defaultValue={quantity}
+                className="input input-bordered w-full "
+              />
+            </div>
+          </div>
+          {/* form row */}
+          <div className="md:flex">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Suplier Name</span>
+              </label>
+              <input
+                type="text"
+                name="suplier"
+                placeholder="Suplier"
+                defaultValue={suplier}
+                className="input input-bordered w-full "
+              />
+            </div>
+            <div className="form-control md:w-1/2 ml-4">
+              <label className="label">
+                <span className="label-text">Taste</span>
+              </label>
+              <input
+                type="text"
+                name="taste"
+                placeholder="Taste"
+                defaultValue={taste}
+                className="input input-bordered w-full "
+              />
+            </div>
+          </div>
+          {/* form row */}
+          <div className="md:flex">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Category</span>
+              </label>
+              <input
+                type="text"
+                name="category"
+                placeholder="Category"
+                defaultValue={category}
+                className="input input-bordered w-full "
+              />
+            </div>
+            <div className="form-control md:w-1/2 ml-4">
+              <label className="label">
+                <span className="label-text">Details</span>
+              </label>
+              <input
+                type="text"
+                name="details"
+                placeholder="Details"
+                defaultValue={details}
+                className="input input-bordered w-full "
+              />
+            </div>
+          </div>
+          {/* form row */}
+          <div className="">
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Photo URL"
+                defaultValue={photo}
+                className="input input-bordered w-full "
+              />
+            </div>
+            <input
+              type="submit"
+              className="btn btn-block mt-6 bg-black text-white"
+              value="UPDATE COFFEE"
+            />
+          </div>
+        </form>
+      </div>
     );
 };
 
